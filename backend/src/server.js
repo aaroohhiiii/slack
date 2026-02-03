6a616a7b9d8f3934da8287c4a0397310a6e2ddc5
Aarohi Gattewar <aarohigattewar@Aarohis-MacBook-Air.local>
2026-02-03 23:38:22 +0530

    db connected & env.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 
+import {ENV} from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
-
-
+const PORT = process.env.PORT || 5001;
 app.get("/" ,(req,res)=>{
     res.send("Hello World");
 } )
-app.listen( 5001, ()=>{
-    console.log("Server is running on port 5001");
+
+await connectDB();
+app.listen( PORT, ()=>{
+    console.log("Server is running on port",PORT);
 })
