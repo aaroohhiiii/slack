5eb99b56dbca6cd6a13b3dad2fdc78885c204a6c
Aarohi Gattewar <aarohigattewar@Aarohis-MacBook-Air.local>
2026-02-06 22:10:07 +0530

    solved deployemnt error

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,13 +22,17 @@ app.get("/" ,(req,res)=>{
 const startServer = async()=>{
     try{
 await connectDB();
-      if(ENV.NODE_ENV !== "PRODUCTION"){
+      if(ENV.NODE_ENV !== "production"){
         app.listen(ENV.PORT , ()=>{
             console.log(`Server is running on port ${PORT}`);
         })
       }
     }catch(error){
-   console.error("error startung server" , error);
+   console.error("error starting server" , error);
    process.exit(1);
     }
 }
+
+startServer();
+
+export default app;
