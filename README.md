21bb3adabe8eadd07af5b5434c2eda201f641a6b
Aarohi Gattewar <aarohigattewar@Aarohis-MacBook-Air.local>
2026-02-07 13:33:28 +0530

    added stream an sentry

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ in env file the port=5001 and nit port = 5001
 
 
 if we jut write app.listen in our server.js it wil llisten foever and vercel doesn want you to dp that 
+
+
+when we create ir delete user we beened tqo tell that to stream too so it can manage the dsta od the user thats wh stream.hs is created 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,12 @@
+
+import "../instrument.mjs";
 import express from "express";
 import {clerkMiddleware} from "@clerk/express";
 import {ENV} from "./config/env.js";
+import * as Sentry from "@sentry/node";
 import { connectDB } from "./config/db.js";
 import {functions , inngest} from "./config/inngest.js";
+import chatRoutes from "./routes/chat.route.js";
 import { serve } from "inngest/express";
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -10,12 +14,20 @@ const PORT = process.env.PORT || 5001;
 //req ath will be availavle in request object 
 app.use(express.json());
 app.use(clerkMiddleware())
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+app.get("/debug-sentry" , (req,res)=>{
+    throw new Error("This is a test error for Sentry debugging")
+})
 
 app.get("/" ,(req,res)=>{
     res.send("Hello World");
     
 } )
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("/api/chat" , chatRoutes)
+Sentry.setupExpressErrorHandler(app);
 
 
 
