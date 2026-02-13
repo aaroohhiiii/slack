93db25c4dbe809d96291ca3946c58d5ad1d034a9
Aarohi Gattewar <aarohigattewar@Aarohis-MacBook-Air.local>
2026-02-13 22:56:31 +0530

    implemented styling for authpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react'
+import AuthPage from './pages/AuthPage'
+import HomePage from './pages/HomePage'
+import {Navigate, Route, Routes} from "react-router"
 const App = () => {
   return (
      <header>
-      {/* Show the sign-in and sign-up buttons when the user is signed out */}
+         <SignedIn>
+        <Routes>
+          <Route path = "/" element = {<HomePage/>} />
+          <Route path = "/auth" element = {<Navigate to = {"/"} replace/>} />
+
+
+
+        </Routes>
+      </SignedIn>
+
       <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton />
+        <Routes>
+          <Route path = "/auth" element = {<AuthPage/>} />
+          <Route path = "*" element = {<Navigate to = {"/auth"} replace/>} />
+          </Routes>
       </SignedOut>
       {/* Show the user button when the user is signed in */}
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+   
     </header>
   )
 }
