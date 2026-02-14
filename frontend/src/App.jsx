c991aeaa0bff2e56708bd2bae056beb69294fa39
Aarohi Gattewar <aarohigattewar@Aarohis-MacBook-Air.local>
2026-02-14 12:44:22 +0530

    implem authprovider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,29 @@ import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@cl
 import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
 import {Navigate, Route, Routes} from "react-router"
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
+
 const App = () => {
+
+
   return (
      <header>
          <SignedIn>
-        <Routes>
+        <SentryRoutes>
           <Route path = "/" element = {<HomePage/>} />
           <Route path = "/auth" element = {<Navigate to = {"/"} replace/>} />
 
 
 
-        </Routes>
+        </SentryRoutes>
       </SignedIn>
 
       <SignedOut>
-        <Routes>
+        <SentryRoutes>
           <Route path = "/auth" element = {<AuthPage/>} />
           <Route path = "*" element = {<Navigate to = {"/auth"} replace/>} />
-          </Routes>
+          </SentryRoutes>
       </SignedOut>
       {/* Show the user button when the user is signed in */}
    
